ebc89a2584b256181d8e649ed33899405b87af8e
jalengaden <jdalengaden@gmail.com>
2025-05-25 21:26:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
 
+// Serve static files from current directory
+app.use(express.static(path.join(__dirname)));
+
+// Serve index.html on root
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
